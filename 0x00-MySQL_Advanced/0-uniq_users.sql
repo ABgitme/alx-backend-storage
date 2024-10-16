08d741a1f995e1b21bb368cce55613ae5010c151
ABgitme <abebekezhabesha@gmail.com>
2024-10-16 22:25:20 +0100

    0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,9 +1,8 @@
-
+-- 'SERIAL' makes the id auto-incremented and sets it as the primary key
+-- 'UNIQUE' ensures that the email is unique in the table
+-- No NOT NULL constraint on name, it can be NULL
 CREATE TABLE IF NOT EXISTS users (
-    -- 'SERIAL' makes the id auto-incremented and sets it as the primary key
     id SERIAL PRIMARY KEY,
-    -- 'UNIQUE' ensures that the email is unique in the table
     email VARCHAR(255) NOT NULL UNIQUE,
-    -- No NOT NULL constraint on name, it can be NULL
     name VARCHAR(255)
 );
