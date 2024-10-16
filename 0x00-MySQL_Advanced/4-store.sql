d568a6a1c5f0af04e44969dd1c3322ccf706b5f3
ABgitme <abebekezhabesha@gmail.com>
2024-10-16 23:26:18 +0100

    4-store.sql

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -2,15 +2,11 @@
 -- Update the quantity of the ordered item in the items table
 -- Decrease the quantity by the amount ordered
 -- Match the item by item_id in the order
-DELIMITER $$
-
 CREATE TRIGGER decrease_quantity_after_order
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
     UPDATE items
-    SET quantity = quantity - NEW.quantity_ordered
-    WHERE id = NEW.item_id;
+    SET quantity = quantity - NEW.number
+    name=NEW.item_name;
 END$$
-
-DELIMITER ;
