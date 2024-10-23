cf032b70d3b72939d4779bc5900881d2f462da6b
ABgitme <abebekezhabesha@gmail.com>
2024-10-23 10:00:27 +0100

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,6 +2,7 @@
 import redis
 import uuid
 from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -38,3 +39,57 @@ class Cache:
 
         # Return the key
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> \
+            Union[str, bytes, int, float, None]:
+        """
+        Retrieve data from Redis and optionally apply a conversion function.
+
+        Args:
+            key (str): The key to retrieve data from Redis.
+            fn (Optional[Callable]): A callable used to convertthe
+            data back to the desired format.
+
+        Returns:
+            Union[str, bytes, int, float, None]: The retrieved data
+            in its original or converted format.
+        """
+        # Get the data from Redis
+        data = self._redis.get(key)
+
+        # If the key does not exist, return None
+        if data is None:
+            return None
+
+        # If a conversion function is provided, apply it
+        if fn:
+            return fn(data)
+
+        # Otherwise, return the raw data (as bytes)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieve a string from Redis.
+
+        Args:
+            key (str): The key to retrieve data from Redis.
+
+        Returns:
+            Optional[str]: The retrieved string,
+            or None if the key doesn't exist.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieve an integer from Redis.
+
+        Args:
+            key (str): The key to retrieve data from Redis.
+
+        Returns:
+            Optional[int]: The retrieved integer,
+            or None if the key doesn't exist.
+        """
+        return self.get(key, fn=int)
